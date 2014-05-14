f2cdbc1ad9c990fbdd09b058d861a61c8adbe3ae
Foelthanos <foelthan@yahoo.fr>
2014-05-14 17:02:58 +0200

    Ajout du code de base pour le projet de jeu. Signed-off-by: Foelthanos <foelthan@yahoo.fr>

---
+++ GameProject/desktop/src/s6/prog6/obichouvine/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package s6.prog6.obichouvine.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import s6.prog6.obichouvine.Obichouvine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Obichouvine(), config);
+	}
+}
---
+++ GameProject/html/src/s6/prog6/obichouvine/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package s6.prog6.obichouvine.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import s6.prog6.obichouvine.Obichouvine;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Obichouvine();
+        }
+}
