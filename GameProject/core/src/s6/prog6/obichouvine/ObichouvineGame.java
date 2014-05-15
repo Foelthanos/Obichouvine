77adcee36a309894beaa763ac0621706e5ef691f
Foelthanos <foelthan@yahoo.fr>
2014-05-15 11:14:02 +0200

    Ajout du MusicManager Signed-off-by: Foelthanos <foelthan@yahoo.fr>

--- GameProject/core/src/s6/prog6/obichouvine/ObichouvineGame.java
+++ GameProject/core/src/s6/prog6/obichouvine/ObichouvineGame.java
@@ -1,6 +1,7 @@
 package s6.prog6.obichouvine;
 
 import s6.prog6.obichouvine.controllers.LevelManager;
+import s6.prog6.obichouvine.controllers.MusicManager;
 import s6.prog6.obichouvine.controllers.PreferencesManager;
 import s6.prog6.obichouvine.screens.MenuScreen;
 import s6.prog6.obichouvine.screens.SplashScreen;
@@ -12,94 +13,105 @@ import com.badlogic.gdx.graphics.FPSLogger;
 
 public class ObichouvineGame extends Game {
 	private FPSLogger fps;
-	
+
 	public static final String LOG = ObichouvineGame.class.getSimpleName();
 	public static final boolean DEV_MODE = true;
 	public static final String VER = "v0.0.3";
-	
+
 	private PreferencesManager preferencesManager;
-    //private ProfileManager profileManager;
-    private LevelManager levelManager;
-    /*private MusicManager musicManager;
-    private SoundManager soundManager;*/ 
+	//private ProfileManager profileManager;
+	private LevelManager levelManager;
+	private MusicManager musicManager;
+	/*private SoundManager soundManager;*/ 
 
 	public PreferencesManager getPreferencesManager()
-    {
-        return preferencesManager;
-    }
-	
+	{
+		return preferencesManager;
+	}
+
 	public LevelManager getLevelManager()
-    {
-        return levelManager;
-    }
-	
+	{
+		return levelManager;
+	}
+
+	public MusicManager getMusicManager()
+	{
+		return musicManager;
+	}
+
 	@Override
 	public void create () {
 		Gdx.app.log(ObichouvineGame.LOG, "Creating game on " + Gdx.app.getType());
-		
+
+		// create the music manager
+        musicManager = new MusicManager();
+        musicManager.setVolume( preferencesManager.getVolume() );
+        musicManager.setEnabled( preferencesManager.isMusicEnabled() );
+
+        
 		// create the preferences manager
 		preferencesManager = new PreferencesManager();
-		
+
 		// create the level manager
-        levelManager = new LevelManager();
-        
+		levelManager = new LevelManager();
+
 		fps = new FPSLogger();	
 	}
 
-	
+
 	@Override
 	public void dispose() {
 		// remember dispose the current screen
 		getScreen().dispose();
-		
+
 		super.dispose();
-		
+
 		// You can use this function to print stuff to the console. 
 		// It's very useful to use to track what's happening in your game.
 		Gdx.app.log(ObichouvineGame.LOG, "Disposed");
 	}
-	
-	@Override
-    public void resize(
-        int width,
-        int height )
-    {
-        super.resize( width, height );
-        Gdx.app.log( ObichouvineGame.LOG, "Resizing game to: " + width + " x " + height );
 
-        // show the splash screen when the game is resized for the first time;
-        // this approach avoids calling the screen's resize method repeatedly
-        if( getScreen() == null ) {
-            if( DEV_MODE ) {
-                setScreen( new MenuScreen( this ) );
-            } else {
-                setScreen( new SplashScreen( this ) );
-            }
-        }
-    }
-	
+	@Override
+	public void resize(
+			int width,
+			int height )
+	{
+		super.resize( width, height );
+		Gdx.app.log( ObichouvineGame.LOG, "Resizing game to: " + width + " x " + height );
+
+		// show the splash screen when the game is resized for the first time;
+		// this approach avoids calling the screen's resize method repeatedly
+		if( getScreen() == null ) {
+			if( DEV_MODE ) {
+				setScreen( new MenuScreen( this ) );
+			} else {
+				setScreen( new SplashScreen( this ) );
+			}
+		}
+	}
+
 	@Override
 	public void render () {
 		super.render();
 		if( DEV_MODE ) fps.log();
 	}
-	
-	@Override
-    public void resume(){
-        super.resume();
-        Gdx.app.log( ObichouvineGame.LOG, "Resuming game" );
-    }
-	@Override
-    public void setScreen(Screen screen ){
-        super.setScreen( screen );
-        Gdx.app.log( ObichouvineGame.LOG, "Setting screen: " + screen.getClass().getSimpleName() );
-    }
 
-	
+	@Override
+	public void resume(){
+		super.resume();
+		Gdx.app.log( ObichouvineGame.LOG, "Resuming game" );
+	}
+	@Override
+	public void setScreen(Screen screen ){
+		super.setScreen( screen );
+		Gdx.app.log( ObichouvineGame.LOG, "Setting screen: " + screen.getClass().getSimpleName() );
+	}
+
+
 	public void pause(){
-        super.pause();
-        Gdx.app.log( ObichouvineGame.LOG, "Pausing game" );
+		super.pause();
+		Gdx.app.log( ObichouvineGame.LOG, "Pausing game" );
 
-    }
+	}
 
 }
