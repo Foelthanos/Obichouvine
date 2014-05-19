978a59db7d5bef00cf48518acfb1907cedeb1f13
Foelthanos <foelthan@yahoo.fr>
2014-05-19 19:24:32 +0200

    Legere modifs Signed-off-by: Foelthanos <foelthan@yahoo.fr>

--- GameProject/core/src/s6/prog6/obichouvine/GameScreen.java
+++ GameProject/core/src/s6/prog6/obichouvine/GameScreen.java
@@ -1,5 +1,7 @@
 package s6.prog6.obichouvine;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
 import s6.prog6.obichouvine.controllers.GameController;
 import s6.prog6.obichouvine.controllers.GameRenderer;
 import s6.prog6.obichouvine.screens.AbstractScreen;
@@ -11,7 +13,18 @@ public class GameScreen extends AbstractScreen{
 	public GameScreen(ObichouvineGame game) {
 		super(game);
 		// TODO Auto-generated constructor stub
+		this.gController = new GameController();
+		this.gRenderer = new GameRenderer();
 	}
 
+	public void show(){
+		super.show();
+		
+		Table table = super.getTable();
+		table.add("Joueur 1 : Joueur 2").spaceBottom( 50 ).colspan(4);
+		table.row();
+		
+		
+	}
 	
 }
