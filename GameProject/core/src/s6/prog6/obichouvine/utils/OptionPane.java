6b1ab7dbcc437bf48af0753ceedb237b6a0d7fda
Foelthanos <foelthan@yahoo.fr>
2014-05-19 12:19:11 +0200

    Ajout du panneau pour le mode spectateur Signed-off-by: Foelthanos <foelthan@yahoo.fr>

--- GameProject/core/src/s6/prog6/obichouvine/utils/OptionPane.java
+++ GameProject/core/src/s6/prog6/obichouvine/utils/OptionPane.java
@@ -1,10 +1,14 @@
 package s6.prog6.obichouvine.utils;
 
+import com.badlogic.gdx.scenes.scene2d.ui.List;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.utils.Array;
 
 public class OptionPane extends Table{
-
+	private Skin skin;
 	
 	public enum Content{
 		IAvsIA, PvsIA, PvsP
@@ -12,6 +16,7 @@ public class OptionPane extends Table{
 	
 	public OptionPane(Content content, Skin skin){
 		super(skin);
+		this.skin = skin;
 		this.updateContent(content);
 	}
 	
@@ -27,9 +32,14 @@ public class OptionPane extends Table{
 
 	private void printPvsP() {
 		// TODO Auto-generated method stub
-		this.add("Parametres de la partie : Joueur vs Joueur").spaceBottom(50).spaceLeft(30);
-		this.top().left();
+		
+		
+		this.add("Parametres de la partie : Joueur vs Joueur").spaceBottom(50).spaceLeft(30).colspan(2);
 		this.row();
+		
+		
+		
+	
 	}
 
 	private void printPvsIA() {
@@ -41,9 +51,32 @@ public class OptionPane extends Table{
 
 	private void printIAvsIA() {
 		// TODO Auto-generated method stub
-		this.add("Parametres de la partie : IA vs IA").spaceBottom(50).spaceLeft(30);
-		this.top().left();
+		List<String> ia1 = new List<String>(skin);
+		List<String> ia2 = new List<String>(skin);
+		Array<String> algo= new Array<String>();
+		algo.add("Facile");
+		algo.add("Moyen");
+		
+		TextButton validate = new TextButton("Valider", skin);
+		
+		ia1.setItems(algo);
+		ia2.setItems(algo);
+		
+		this.add("Parametres de la partie : IA vs IA").spaceBottom(50).spaceLeft(30).expandX().colspan(2);
 		this.row();
+		
+		this.add("Difficulté IA 1 :").spaceBottom(30).fillX().center().expandY();
+		this.add(ia1).uniform().spaceBottom(30).fillX().expandY();
+		this.row();
+		
+		this.add("Difficulté IA 2 :").spaceBottom(30).fillX().center().expandY();
+		this.add(ia2).uniform().spaceBottom(30).fillX().expandY();
+		this.row();
+		
+		this.add(validate).colspan(2).fillX();
+		this.row();
+		
+		
 	}
 	
 }
